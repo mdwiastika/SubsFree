4749b41706cabc9112166c59f639d57ff812ca2d
Marcel Dwi Astika <marceldwias@gmail.com>
2023-12-10 20:41:45 +0700

    update dashboard

--- resources/views/admin/page/dashboard/main.blade.php
+++ resources/views/admin/page/dashboard/main.blade.php
@@ -22,32 +22,34 @@
       <div class="container-fluid">
          <div class="row">
             @if (Auth::check() && auth()->user()->level_user != 'Pengguna')
-               <div class="col-lg-6 col-12">
-                  <!-- small box -->
-                  <div class="small-box" style="background-color: #3D3C4C; color: white;">
-                     <div class="inner">
-                        <h3>Pengguna</h3>
-                        <h4>{{ $pengguna_count }}</h4>
+               @if (in_array(Auth::user()->level_user, ['Super Admin', 'Admin']))
+                  <div class="col-lg-6 col-12">
+                     <!-- small box -->
+                     <div class="small-box" style="background-color: #3D3C4C; color: white;">
+                        <div class="inner">
+                           <h3>Pengguna</h3>
+                           <h4>{{ $pengguna_count }}</h4>
+                        </div>
+                        <div class="icon">
+                           <i class="fas fa-users" style="color: white"></i>
+                        </div>
+                        <a href="{{ route('users') }}" class="small-box-footer">Lihat Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
                      </div>
-                     <div class="icon">
-                        <i class="fas fa-users" style="color: white"></i>
-                     </div>
-                     <a href="{{ route('users') }}" class="small-box-footer">Lihat Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
                   </div>
-               </div>
-               <div class="col-lg-6 col-12">
-                  <!-- small box -->
-                  <div class="small-box" style="background-color: #f78c00; color: white;">
-                     <div class="inner">
-                        <h3>Mitra</h3>
-                        <h4>{{ $mitra_count }}</h4>
+                  <div class="col-lg-6 col-12">
+                     <!-- small box -->
+                     <div class="small-box" style="background-color: #f78c00; color: white;">
+                        <div class="inner">
+                           <h3>Mitra</h3>
+                           <h4>{{ $mitra_count }}</h4>
+                        </div>
+                        <div class="icon">
+                           <i class="fas fa-handshake" style="color: white"></i>
+                        </div>
+                        <a href="{{ route('users') }}" class="small-box-footer">Lihat Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
                      </div>
-                     <div class="icon">
-                        <i class="fas fa-handshake" style="color: white"></i>
-                     </div>
-                     <a href="{{ route('users') }}" class="small-box-footer">Lihat Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
                   </div>
-               </div>
+               @endif
                <div class="col-lg-6 col-12">
                   <!-- small box -->
                   <div class="small-box" style="background-color: #00f70c; color: white;">
@@ -58,7 +60,7 @@
                      <div class="icon">
                         <i class="fas fa-hand-holding-usd" style="color: white"></i>
                      </div>
-                     <a href="{{ route('users') }}" class="small-box-footer">Lihat Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
+                     <a href="{{ route('transactionRoom') }}" class="small-box-footer">Lihat Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
                   </div>
                </div>
             @endif
